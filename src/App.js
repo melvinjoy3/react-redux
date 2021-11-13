299a766118781dba4bc9eac98abd85896f121a72
melbin joy <melvinjoy8@gmail.com>
2021-11-13 09:57:17 +0530

    demo commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
