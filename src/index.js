ed96f68705a223908378cc7567ec091ad6e5e9da
melbin joy <melvinjoy8@gmail.com>
2021-11-17 15:56:04 +0530

    Provider Wrapped in index and add redux folder

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from './Redux/Store/Store'
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
+  <Provider Store={Store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
