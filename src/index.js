0d889db0d32198bc7ad739318d62c87b63dabb9f
melbin joy <melvinjoy8@gmail.com>
2021-11-17 16:01:56 +0530

    store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Store from './Redux/Store/Store'
+import store from './Redux/Store/Store'
 import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <Provider Store={Store}>
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
